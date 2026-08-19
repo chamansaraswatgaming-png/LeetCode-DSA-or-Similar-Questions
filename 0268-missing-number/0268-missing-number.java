699ac3d4089c54ae04730f9cd364dbf153e3324e
chamansaraswatgaming-png <chamansaraswatgaming@gmail.com>
2026-08-19 17:04:49 +0530

    Time: 0 ms (100%), Space: 47.4 MB (49.67%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,26 @@
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         Arrays.sort(nums);
+//          int i=0;
+//          while (i<nums.length){
+//             if(nums[i]!=i){
+//             return i;
+//             }
+//             else i++;
+//          }
+//      return i;   
+//     }
+// }
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-         int i=0;
-         while (i<nums.length){
-            if(nums[i]!=i){
-            return i;
-            }
-            else i++;
-         }
-     return i;   
+
+        int xor = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= i;
+            xor ^= nums[i];
+        }
+
+        return xor;
     }
 }
